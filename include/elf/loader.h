9a44c8dceafb940f6fab45898167243fee59b1b9
Hackerl <490021209@qq.com>
2022-08-11 11:33:25 +0800

    Initial commit

---
+++ include/elf/loader.h
@@ -0,0 +1,26 @@
+#ifndef ELF_LOADER_LOADER_H
+#define ELF_LOADER_LOADER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uintptr_t base;
+    uintptr_t entry;
+    uintptr_t header;
+    size_t header_num;
+    size_t header_size;
+} elf_context_t;
+
+int load_elf(const char *path, elf_context_t ctx[2]);
+int jump_to_entry(elf_context_t ctx[2], int argc, char **argv, char **envp);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ELF_LOADER_LOADER_H
